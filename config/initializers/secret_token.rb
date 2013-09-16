a01f0e70b45a328a3fcc965d900da29ac25ee5f9
Ed Mills <molarexplorer@yahoo.co.uk>
2013-09-16 23:14:42 +0100

    authentication crash

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'bd73b4eba4995728c2ef585e7818fd261f7751d3f046dff779e113cfe1717d129ecf6dd39b33bf173fb26c60bfd7b43d4e2d63c2654d9780b290dda09c252fbc'
